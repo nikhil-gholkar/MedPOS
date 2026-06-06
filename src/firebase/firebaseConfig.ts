97ea1a130c3804e4759cf89ea9b33dc5c34254f5
Nikhil Gholakar <nikhil.gholkar76@gmail.com>
2026-06-06 18:16:04 +0530

    bug fixes

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- apiKey: "AIzaSyBifTBcJweN55ed8u0XuivI2vJ4Zqmq6V0",
-  authDomain: "medpos-webapp.firebaseapp.com",
-  projectId: "medpos-webapp",
-  storageBucket: "medpos-webapp.firebasestorage.app",
-  messagingSenderId: "503616103388",
-  appId: "1:503616103388:web:0a0a5fce188a6d9708e519"
+ apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env. VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.ITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
